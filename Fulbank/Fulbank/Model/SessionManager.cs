8c840d7143461f9c7d61e202e189ec6adee2d852
NoaKorogu <noa.guignolle@lyceefulbert.fr>
2024-11-13 16:52:06 +0100

    Ajout de SessionManager.cs

---
+++ Fulbank/Fulbank/Model/SessionManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fulbank.Model
+{
+    internal class SessionManager
+    {
+        public static int CurrentUser { get; set; }
+    }
+}
